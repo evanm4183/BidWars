a36f5bd99436e8327303ca2feb13be82289d6307
evanm4183 <evanm4183@gmail.com>
2025-08-03 15:45:58 -0500

    mvc project template

---
+++ BidWars/BidWars/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BidWars.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
