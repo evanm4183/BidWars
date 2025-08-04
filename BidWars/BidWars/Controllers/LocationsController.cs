d9f5ada840837c1ab3110246c9f5328135171674
evanm4183 <evanm4183@gmail.com>
2025-08-03 20:12:10 -0500

    locations

---
+++ BidWars/BidWars/Controllers/LocationsController.cs
@@ -0,0 +1,45 @@
+﻿using BidWars.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BidWars.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class LocationsController : Controller
+    {
+        private readonly BidWarsContext _context;
+
+        public LocationsController(BidWarsContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var locations = await _context.Locations.ToListAsync();
+                
+            return View(locations);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BidWars/BidWars/Database/Entities/Location.cs
+++ BidWars/BidWars/Database/Entities/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BidWars.Database;
 
@@ -7,13 +8,19 @@ public partial class Location
 {
     public int Id { get; set; }
 
+    [Display(Name = "Street Address")]
     public string StreetAddress { get; set; } = null!;
 
     public string City { get; set; } = null!;
 
+    [StringLength(2)]
     public string State { get; set; } = null!;
 
+    [StringLength(5)]
     public string Zip { get; set; } = null!;
+
+    [Display(Name = "Location Name")]
+    public string LocationName { get; set; } = null!;
 
     public virtual ICollection<Auction> Auctions { get; set; } = new List<Auction>();
 }
